3bd561a684b122e3cacbdb0fde5757ba7a0a8f64
santiagogiraldo128 <santiagogiraldo128@gmail.com>
2023-08-17 23:01:58 -0500

    Segunda prueba

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -23,7 +23,7 @@ public class Auto {
         if (registro  == motor.registro) {
             for (int i=0; i<asientos.length; i++) {
                 if (asientos[i] != null) {
-                    if (asientos[i].registro == registro) {
+                    if (asientos[i].registro != registro) {
                         return "Las piezas no son originales";
                     }
                 }
